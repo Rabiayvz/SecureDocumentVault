b3f985d3c936d5083ae843b6bc494f3e7cf7e371
rabiayvz <rabiayvz38@gmail.com>
2026-04-05 16:43:58 +0300

    Add basic DTO and controller examples

---
+++ FirstApi/Controllers/ApiStatus.cs
@@ -0,0 +1,40 @@
+﻿using FirstApi.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiStatusController : ControllerBase
+    {
+
+        private static List<ProductDto> _products = new List<ProductDto>();
+
+        [HttpGet(Name = "GetApiStatus")]
+
+        public IActionResult Get() {
+            return Ok(new {message = "Controller is working 🚀"});
+        }
+
+        [HttpGet("GetProducts")]
+
+        public IActionResult GetProducts()
+        {
+            return Ok(_products);
+        }
+
+        [HttpPost(Name = "PostApiStatus")]
+
+        public IActionResult Create([FromBody] ProductDto dto)
+        {
+            _products.Add(dto);
+
+            return Ok(new
+            {
+                message = "Veri alındı",
+                data = dto
+            });
+        }
+
+    }
+}
---
+++ FirstApi/Dtos/ProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstApi.Dtos
+{
+    public class ProductDto
+    {
+        public string name { get; set; }
+        public decimal price { get; set; }
+    }
+}
