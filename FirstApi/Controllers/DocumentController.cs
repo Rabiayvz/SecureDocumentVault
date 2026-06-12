396bd743029cd6f7f9e930b6b7245e1bd221c5a5
Rabia <rabiayvz38@gmail.com>
2026-06-12 17:28:29 +0300

    controller added

--- FirstApi/Controllers/DocumentController.cs
+++ FirstApi/Controllers/DocumentController.cs
@@ -4,8 +4,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FirstApi.Controllers
 {
-    public class DocumentController
+    [ApiController]
+    [Route("documents")]
+    public class DocumentController : ControllerBase
     {
+        private readonly DocumentService _documentService;
+        public DocumentController(DocumentService documentService)
+        {
+            _documentService = documentService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateDocument(CreateDocumentDto dto)
+        {
+            var documentId = _documentService.CreateDocument(dto);
+
+            return Ok(documentId);
+        }
+
+        [HttpGet]
+        public IActionResult GetDocuments()
+        { 
+            var documents = _documentService.GetDocuments();
+
+            return Ok(documents);
+        }
+
+        [HttpGet]
+
+        public IActionResult GetDocument(Guid id) {
 
     }
 }
